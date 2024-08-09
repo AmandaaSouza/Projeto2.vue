b10ac9cb6d2926c8605b24c5b9d46860c587c752
AmandaaSouza <161317868+AmandaaSouza@users.noreply.github.com>
2024-08-09 11:25:08 -0300

    Add files via upload

--- src/components/PaginaPrincipal.vue
+++ src/components/PaginaPrincipal.vue
@@ -4,15 +4,15 @@ ola
 <br>
 <VideoUnico/>
 <br>
-<TabelasDiversos/>
+<TabelaDiversos/>
 <br>
 
 </template>
 
 <script>
 import ImagemPrincipal from '@/components/ImagensDiversas.vue'
-import VideoUnico from './VideoUnico.vue'
-import TabelaDiversos from './TabelaDiversos.vue';
+import VideoUnico from '@/components/VideoUnico.vue'
+import TabelaDiversos from '@/components/TabelaDiversos.vue';
 export default {
     name: 'PaginaPrincipal',
     components: {
